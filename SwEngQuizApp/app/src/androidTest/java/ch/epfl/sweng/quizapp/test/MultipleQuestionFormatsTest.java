5b6e3efd7f55cfc3949dcc82020d6c8b1f68cd10
MANQUAT Gautier <gautier.manquat@epfl.ch>
2015-10-09 14:53:56 +0200

    #task8

--- SwEngQuizApp/app/src/androidTest/java/ch/epfl/sweng/quizapp/test/MultipleQuestionFormatsTest.java
+++ SwEngQuizApp/app/src/androidTest/java/ch/epfl/sweng/quizapp/test/MultipleQuestionFormatsTest.java
@@ -54,8 +54,21 @@ public class MultipleQuestionFormatsTest
             + "  ],\n"
             + "  \"owner\": \"sweng\"\n"
             + "}\n";
+    private static final String MARDOWN_RESPONSE = "What is the answer to life, the universe "
+            + "and everything? {#17005 owner=sweng}\n"
+            + "\n"
+            + "* We don't know\n"
+            + "* 42\n"
+            + "* []{.icon-ok} six times nine\n"
+            + "  (if you compute in base 13)\n"
+            + "* The mice know,\n"
+            + "  but they won't tell.";
+
     private static final String JSON_CONTENT_TYPE = "application/json; charset=utf-8";
     private static final String TEXT_CONTENT_TYPE = "text/plain; charset=utf-8";
+    private static final String PDF_CONTENT_TYPE = "text/pdf";
+    private static final String MARKDOWN_CONTENT_TYPE = "text/markdown";
+
 
     private NetworkQuizClient quizClient;
     private HttpURLConnection connection;
@@ -108,7 +121,7 @@ public class MultipleQuestionFormatsTest
     @Test
     public void testQuestionParsingPDF() throws IOException
     {
-        configureResponse(HttpURLConnection.HTTP_OK, JSON_RESPONSE, "text/pdf");
+        configureResponse(HttpURLConnection.HTTP_OK, JSON_RESPONSE, PDF_CONTENT_TYPE);
         try
         {
             quizClient.fetchRandomQuestion();
@@ -118,5 +131,32 @@ public class MultipleQuestionFormatsTest
         {
             //good
         }
+    }
+
+    /**
+     * Test that the constructed quiz question is correct when a Markdown file is given.
+     * @throws QuizClientException
+     * @throws IOException
+     */
+    @Test
+    public void testQuestionParsingMarkDown() throws QuizClientException, IOException
+    {
+        configureResponse(HttpURLConnection.HTTP_OK, MARDOWN_RESPONSE, MARKDOWN_CONTENT_TYPE);
+        QuizQuestion question = quizClient.fetchRandomQuestion();
+
+        List<String> answers = new ArrayList<>();
+        answers.add("We don't know");
+        answers.add("42");
+        answers.add("six times nine (if you compute in base 13)");
+        answers.add("The mice know, but they won't tell.");
+
+        List<String> tags = new ArrayList<>();
+
+        assertEquals(SAMPLE_QUESTION_ID, question.getID());
+        assertEquals("What is the answer to life, the universe and everything?", question.getBody());
+        assertEquals(answers, question.getAnswers());
+        assertEquals(3, question.getSolutionIndex());
+        assertEquals(tags, question.getTags());
+        assertEquals("sweng", question.getOwner());
     }
 }
