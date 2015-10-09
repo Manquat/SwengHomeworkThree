ffa18d67899d3cafdc6c1968709f15cb9375be7f
MANQUAT Gautier <gautier.manquat@epfl.ch>
2015-10-09 17:45:15 +0200

    passe the test with the answer, index of correct answer and tags(void)

--- SwEngQuizApp/app/src/main/java/ch/epfl/sweng/quizapp/MarkDownQuizQuestionParser.java
+++ SwEngQuizApp/app/src/main/java/ch/epfl/sweng/quizapp/MarkDownQuizQuestionParser.java
@@ -3,10 +3,13 @@ package ch.epfl.sweng.quizapp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /** Parses questions in MarkDown format */
 public class MarkDownQuizQuestionParser implements QuizQuestionParser
 {
+    static final String CORRECT_ANSWER_MARK = "[]{.icon-ok}";
 
     @Override
     public QuizQuestion parse(String s) throws QuizQuestionParseException
@@ -28,23 +31,116 @@ public class MarkDownQuizQuestionParser implements QuizQuestionParser
         {
             throw new QuizQuestionParseException ();
         }
-        //String bodyAndAttrs = parts[0];
-        //String answerList = parts[1];
+        String bodyAndAttrs = parts[0];
+        String answerList = parts[1];
 
-        List<String> answers = new ArrayList<>();
+
+        //------------------------------------------------------------------------------------------
+        // treatment of the question's part
+
+        String[] bodyAndAttrsPart = bodyAndAttrs.split("\\{.*\\}");
+
+
+        //------------------------------------------------------------------------------------------
+        // treatment of the answer's part
+
+        String[] answerListString = answerList.split("\\* ");
+        if (answerListString[0].equals(""))
+        {
+            String[] tempString = new String[answerListString.length-1];
+            for (int i=0; i < answerListString.length -1; i++)
+            {
+                tempString[i] = answerListString[i+1];
+            }
+            answerListString = tempString;
+        }
+
+        int indexOfCorrectAnswer = -1;
+
+        for (int i = 0; i < answerListString.length; i++)
+        {
+            answerListString[i] = normalize(answerListString[i]);
+
+            // if this is the correct answer
+            int tempIndex = answerListString[i].indexOf(CORRECT_ANSWER_MARK);
+            if (tempIndex != -1)
+            {
+                indexOfCorrectAnswer = i;
+                answerListString[i] = answerListString[i].substring(CORRECT_ANSWER_MARK.length());
+                answerListString[i] = normalize(answerListString[i]);
+            }
+        }
+        // if there is no correct answer
+        if (indexOfCorrectAnswer == -1)
+        {
+            throw new QuizQuestionParseException();
+        }
+
+        /*List<String> answers = new ArrayList<>();
         answers.add("We don't know");
         answers.add("42");
         answers.add("six times nine (if you compute in base 13)");
-        answers.add("The mice know, but they won't tell.");
+        answers.add("The mice know, but they won't tell.");*/
+        List<String> answers = Arrays.asList(answerListString);
+
+
+
 
         return new QuizQuestion(
                 17005,                                         // ID
                 "sweng",                                  // Owner
                 "What is the answer to life, the universe and everything?",                                       // Question text
                 answers,  // Answers
-                2,                                         // Solution index
+                indexOfCorrectAnswer,                      // Solution index
                 Arrays.asList(new String[] {})             // Tags
                 );
+    }
+
+    private String normalize(String string)
+    {
+        String normalized = string;
+        Boolean findSomething;
+
+        Pattern pAnswerNewLine = Pattern.compile("\\n");
+        Pattern pAnswerDoubleSpace = Pattern.compile("  ");
+
+        do
+        {
+            findSomething = false;
+
+            // find and replace all the \n by a space
+            Matcher m = pAnswerNewLine.matcher(normalized);
+            if (m.find())
+            {
+                normalized = m.replaceAll(" ");
+                findSomething = true;
+            }
+
+            // find and replace all the double space by a space
+            m = pAnswerDoubleSpace.matcher(normalized);
+            if (m.find())
+            {
+                normalized = m.replaceAll(" ");
+                findSomething = true;
+            }
+
+            // if there is a space at the end of the answer pop it
+            if (normalized.startsWith(" "))
+            {
+                normalized = normalized.substring(1);
+                findSomething = true;
+            }
+
+            // if there is a space at the end of the answer pop it
+            if (normalized.endsWith(" "))
+            {
+                normalized = normalized.substring(0, normalized.length() - 1);
+                findSomething = true;
+            }
+        }
+        while (findSomething);
+
+        return normalized;
     }
 
 }
